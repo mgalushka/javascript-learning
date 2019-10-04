ed20aaeea14fa1e894491a300baa863276b9d6b1
mgalushka <mgalushka@gmail.com>
2019-10-04 21:52:29 +0100

    learning javascript quotes

---
+++ src/quotes.js
@@ -0,0 +1,6 @@
+// What is the output of the script?
+let name = "Ilya";
+
+console.log(`hello ${1}` === "hello 1");
+console.log(`hello ${"name"}` === "hello name");
+console.log(`hello ${name}` === "hello Ilya");
