e05d823ad863248c683ba4e1e789904c35b2a5de
Maxim Galushka <mgalushka@gmail.com>
2019-10-04 13:47:24 +0100

    setting up babel

---
+++ src/first/index.js
@@ -0,0 +1,22 @@
+//  Write the code, one line for each action:
+//
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+let user = {};
+console.log(user);
+
+user.name = "John";
+console.log(user);
+
+user.surname = "Smith";
+console.log(user);
+
+user.name = "Pete";
+console.log(user);
+
+delete user.name;
+console.log(user);
