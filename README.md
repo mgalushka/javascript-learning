5e476d9ee00f8984fa35bb23c3ce35d85a24eeb5
Maxim Galushka <mgalushka@gmail.com>
2019-10-05 18:47:27 +0100

    Finilised configuration for burring babel-node

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ My learning experience of javascript
 
 How to run script:
 ```
-node src/first/index.js
+yarn run babel-node src/arrays.js
 ```
 
 How to monitor flow via `flow-watch`: this will constantly monitor code for
@@ -15,4 +15,12 @@ yarn run flow-watch
 How to run jest tests on every change:
 ```
 yarn run jest --watchAll
+```
+
+# Setup instructions
+
+```
+yarn add --dev @babel/core @babel/cli @babel/preset-flow
+yarn add --dev jest
+yarn add --dev @babel/node
 ```
---
+++ src/arrays.js
@@ -0,0 +1,26 @@
+// @flow
+
+/*
+// Write a range function that takes two arguments,
+// start and end, and retuen array containing all the numbers
+// from start up to (and including) end.
+// Next, write a sum function that takes an array of numbers
+// and returns thesum of these numbers.
+// Run the example program and see whether it does indeed return 55.
+*/
+const range = (start: number, end: number) => {
+  let result: number[] = [];
+  if (start < end) {
+    return result;
+  }
+  for (let i=start; i<end; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+const sum = (numbers: number[]) => {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
+
+console.log(sum(range(1, 10)));
