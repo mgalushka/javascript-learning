7bbb062e258cdf6345b0d6c9758fee936533c2b3
mgalushka <mgalushka@gmail.com>
2019-10-26 14:43:35 +0100

    playing with some unsuccessful typing for JS proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,15 +1,24 @@
 // This is type unsfae for now
 // TODO: find out how to type javascript Proxy with flow
 
+type ProxyWrapper = {
+  get: (Object, string) => string,
+}
+
+type EmptyObject = {
+
+}
+
 // Proxy
-var handler = {
-    get: function(obj: Object, prop: string) {
+var handler: Proxy<Object> = {
+    get: (obj: Object, prop: string): string => {
         return prop in obj ?
             obj[prop] :
             "missing";
     }
 };
 
-let p: Proxy<Object> = new Proxy({}, handler);
-console.log(p.someMissingProperty)
+const inputObject: Object = {};
+let p: Proxy<Object> = new Proxy(inputObject, handler);
+console.log((p.someMissingProperty: string))
 console.log(p.someMissingProperty === "missing")
