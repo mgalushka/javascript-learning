94fc1ae6c619d537c73ae42dbf134cee06699810
mgalushka <mgalushka@gmail.com>
2019-10-31 20:51:51 +0000

    various tests

--- test/maps.test.js
+++ test/maps.test.js
@@ -14,3 +14,24 @@ const portfolio: Transaction[] = [
 test('copy transactions test', () => {
   expect(maps.copyTransactions(portfolio)).toEqual(portfolio);
 });
+
+test('test mapByIndex', () => {
+  expect(maps.mapByX(portfolio, 'index').get('MSFT')).toEqual(
+    expect.arrayContaining([
+      {index: "MSFT", date: "2019-03-01", amount: 12, price: 2.44, direction: "SELL"},
+      {index: "MSFT", date: "2019-03-01", amount: 5, price: 2.09, direction: "BUY"},
+      {index: "MSFT", date: "2019-03-01", amount: 7, price: 2.22, direction: "BUY"},
+    ]
+  ));
+});
+
+test('test mapByDate', () => {
+  expect(maps.mapByX(portfolio, 'date').get('2019-03-01')).toEqual(
+    expect.arrayContaining([
+      {index: "MSFT", date: "2019-03-01", amount: 12, price: 2.44, direction: "SELL"},
+      {index: "MSFT", date: "2019-03-01", amount: 5, price: 2.09, direction: "BUY"},
+      {index: "MSFT", date: "2019-03-01", amount: 7, price: 2.22, direction: "BUY"},
+      {index: "APPL", date: "2019-03-01", amount: 1, price: 12.06, direction: "BUY"},
+    ]
+  ));
+});
