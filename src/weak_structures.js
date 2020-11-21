79f8e929a982e7da7e5d9b3878f4d39f0978505e
mgalushka <mgalushka@gmail.com>
2020-11-21 21:40:23 +0000

    Better formatting

--- src/weak_structures.js
+++ src/weak_structures.js
@@ -1,12 +1,20 @@
 // @flow
 /*
- * Your code can access it, but the messages are managed by someone else’s code. New messages are added, old ones are removed regularly by that code, and you don’t know the exact moments when it happens.
+ * Your code can access it, but the messages are managed by someone else’s code.
+ * New messages are added, old ones are removed regularly by that code,
+ * and you don’t know the exact moments when it happens.
  *
- * Now, which data structure you could use to store information whether the message “have been read”? The structure must be well-suited to give the answer “was it read?” for the given message object.
+ * Now, which data structure you could use to store information
+ * whether the message “have been read”?
+ * The structure must be well-suited to give the answer “was it read?”
+ * for the given message object.
  *
- * P.S. When a message is removed from messages, it should disappear from your structure as well.
+ * P.S. When a message is removed from messages,
+ * it should disappear from your structure as well.
  *
- * P.P.S. We shouldn’t modify message objects, add our properties to them. As they are managed by someone else’s code, that may lead to bad consequences.
+ * P.P.S. We shouldn’t modify message objects,
+ * add our properties to them.
+ * As they are managed by someone else’s code, that may lead to bad consequences.
  *
  * https://javascript.info/weakmap-weakset#store-unread-flags
 */
